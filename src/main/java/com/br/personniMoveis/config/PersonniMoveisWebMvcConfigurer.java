d13d4bd14479953ac12f3b1c8fcde957a3443668
Athosmfr <athos.mfrodrigues@gmail.com>
2023-08-22 10:54:40 -0300

    cors2

--- src/main/java/com/br/personniMoveis/config/PersonniMoveisWebMvcConfigurer.java
+++ src/main/java/com/br/personniMoveis/config/PersonniMoveisWebMvcConfigurer.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -23,5 +24,12 @@ public class PersonniMoveisWebMvcConfigurer implements WebMvcConfigurer {
 
         // Configurações adicionadas ao resolver:
         resolver.add(pageHandler);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Inserir porta do front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
