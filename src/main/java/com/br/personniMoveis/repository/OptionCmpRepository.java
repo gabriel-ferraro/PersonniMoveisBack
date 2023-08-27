e9d5793c21ba349012b93605d7d00507ddedff14
Aguinaldo <guimachado1708@gmail.com>
2023-08-27 20:03:43 -0300

    POST de CMP funcionando e getById do Cmp

--- src/main/java/com/br/personniMoveis/repository/OptionCmpRepository.java
+++ src/main/java/com/br/personniMoveis/repository/OptionCmpRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 
 public interface OptionCmpRepository extends JpaRepository<OptionCmp, Long>, JpaSpecificationExecutor<OptionCmp> {
+    Set<OptionCmp> findByElementCmp(Long elementCmp);
 
 }
