38bc8db728e34fb2dcd7c1e752071b1d5e3847aa
Aguinaldo <guimachado1708@gmail.com>
2023-09-23 12:17:29 -0300

    Adicionando permissão CORS

--- src/main/java/com/br/personniMoveis/controller/ElementCmpController.java
+++ src/main/java/com/br/personniMoveis/controller/ElementCmpController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("ElementCmp")
+@CrossOrigin(origins = "*")
 @SecurityRequirement(name = "bearer-key")
 public class ElementCmpController {
 
