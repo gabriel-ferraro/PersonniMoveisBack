522d2f6a4c0a86de5741194cf482dc70367e76cf
Gabriel Ferraro <gabrielsev_2002@hotmail.com>
2023-10-03 00:40:07 -0300

    alteração dtos bloqueados

--- src/main/java/com/br/personniMoveis/dto/product/CategoryDto.java
+++ src/main/java/com/br/personniMoveis/dto/product/CategoryDto.java
@@ -2,11 +2,13 @@ package com.br.personniMoveis.dto.product;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDto {
 
     private Long categoryId;
