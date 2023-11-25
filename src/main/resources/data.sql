26d2c4b306ec49a8503b2e111ae019c7b545e64a
Gabriel Ferraro <gabrielsev_2002@hotmail.com>
2023-11-25 13:54:51 -0300

    correção redirectrs e carregamento de dados inciais funcionando

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
---INSERT INTO user_entity(user_id, cpf, email, "name", is_removed, "password", phone_number, profile)
---VALUES(1, '54817195096', 'admin@gmail.com', 'admin', false, '123', '123', 1);
